623b107648714cafb978661b37cdcf6157516159
Florian Schneider <me@florian-s.de>
2022-09-02 12:50:22 +0200

    Add project files.

---
+++ Ueberweisung/BankAccount.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ueberweisung
+{
+    internal class BankAccount
+    {
+        public object Lock = new object();
+        public decimal Balance { get; private set; }
+        public decimal InitValue { get; private set; }
+        public int IBAN { get; private set; }
+        private string Country { get; set; }
+        public bool Active { get; private set; } = true;
+
+        public BankAccount(decimal balance = 0.0m)
+        {
+            Balance = balance;
+            InitValue = balance;
+            GenerateIBAN();
+            Country = "DE";
+        }
+
+        public string GetIBAN()
+        {
+            return Country + IBAN;
+        }
+
+        public void TransferTo(BankAccount otherAccount, decimal amount)
+        {
+            BankAccount lower, higher;
+            if (otherAccount.IBAN < IBAN)
+            {
+                lower = this;
+                higher = otherAccount;
+            }
+            else
+            {
+                higher = this;
+                lower = otherAccount;
+            }
+
+            lock (lower.Lock)
+            {
+                lock (higher.Lock)
+                {
+                    otherAccount.Balance += amount;
+                    Balance -= amount;
+                }
+            }
+            PrintTransaction(otherAccount, amount);
+        }
+
+        public static void PrintHeader()
+        {
+            Console.WriteLine("{0,13} | {1,13} | {2,6}", "IBAN sender", "IBAN receiver", "Amount");
+            Console.WriteLine("-------------------------------------------");
+        }
+
+        private void PrintTransaction(BankAccount otherAccount, decimal amount)
+        {
+            Console.WriteLine("{0,13} | {1,13} | {2,-8}EUR", this.IBAN, otherAccount.IBAN, amount);
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("IBAN {0} - Balance {1,9} EUR", IBAN, Math.Round(Balance, 2));
+        }
+
+        private void GenerateIBAN()
+        {
+            Random srand = new Random();
+            IBAN = srand.Next(int.MaxValue / 2, int.MaxValue);
+        }
+    }
+}
---
+++ Ueberweisung/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Threading;
+
+namespace Ueberweisung
+{
+    internal class Program
+    {
+        static BankAccount[] bankAccounts = new BankAccount[15];
+        static Thread[] tr = new Thread[15];
+        static void Main(string[] args)
+        {
+            decimal amount = 0m;
+            Random srand = new Random();
+
+            for(int i = 0; i < 15; i++)
+            {
+                bankAccounts[i] = new BankAccount(Convert.ToDecimal(srand.NextDouble() * srand.Next(100,10000)));
+                bankAccounts[i].Print();
+                tr[i] = new Thread(StartTask);
+                tr[i].Start(bankAccounts[i]);
+            }
+            
+            for(int i = 0; i < tr.Length; i++)
+            {
+                tr[i].Join();
+            }
+
+            Thread.Sleep(100);
+            CountValues();
+        }
+
+        private static void CountValues()
+        {
+            decimal totalMoney = 0m;
+            decimal currentMoney = 0m;
+
+            foreach(BankAccount account in bankAccounts)
+            {
+                totalMoney += account.InitValue;
+                currentMoney += account.Balance;
+            }
+
+            Console.WriteLine("Total money: " + Math.Round(totalMoney,2).ToString());
+            Console.WriteLine("Current money: " + Math.Round(currentMoney,2).ToString());
+        }
+
+        private static void StartTask(object state)
+        {
+            if (!(state is BankAccount)) return;
+            BankAccount bankAccount = (BankAccount)state;
+            Random srand = new Random();
+            int current = 0;
+            int max = 100;
+            int random;
+            while(max >= current)
+            {
+                random = srand.Next(0,bankAccounts.Length);
+                if (bankAccounts[random] is null)
+                {
+                    Thread.Sleep(20);
+                    continue;
+                }
+                bankAccount.TransferTo(bankAccounts[random], Convert.ToDecimal(srand.NextDouble() + 0.5 * srand.Next(100,1000)));
+                current++;
+                Thread.Sleep(5);
+            }
+        }
+    }
+}
